da4059a6f3077fd33d2fed85a7f546b28ef6cfcc
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2024-05-09 06:38:40 +0200

    Replace pachage to the root

--- RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import RazeCore
-
-final class RazeCoreTests: XCTestCase {
-    func testColorRedEqual() {
-      let color = RazeCore.colorFromHexString("FF0000")
-      XCTAssertEqual(color, .red)
-    }
-}
