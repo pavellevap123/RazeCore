216999a9171ca753629b52ffbcf6750fdd7e0c97
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2024-05-09 16:24:53 +0200

    Add netwoking

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,6 +9,20 @@ import Foundation
 
 extension RazeCore {
     public class Networking {
-        
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create before using any public APIs
+        public class Manager {
+            public init() {}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler: @escaping (Result<Data, Error>) -> Void) {
+                let task = session.dataTask(with: url) { data, response, error in
+                    guard let data else { return }
+                    completionHandler(.success(data))
+                }
+                task.resume()
+            }
+        }
     }
 }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -1,14 +1,29 @@
 //
 //  RazeNetworkingTests.swift
-//  
+//
 //
 //  Created by Pavel Paddubotski on 9.05.24.
 //
 
 import XCTest
+@testable import RazeCore
 
 final class RazeNetworkingTests: XCTestCase {
-    func testExample() throws {
-        
+    func testLoadDataCall() {
+        let manager = RazeCore.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard let url = URL(string: "https://raywenderlich.com") else {
+            return XCTFail("Could not create URL properly")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success (let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        wait(for: [expectation], timeout: 5)
     }
 }
