46cb0db8f3c35a8c53fe5bd4752503d0e74a9306
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2024-05-09 15:20:23 +0200

    Add preset color

--- Tests/RazeCoreTests/RazeCoreTests.swift
+++ Tests/RazeCoreTests/RazeCoreTests.swift
@@ -7,4 +7,9 @@ final class RazeCoreTests: XCTestCase {
         let color = RazeCore.colorFromHexString("FF0000")
         XCTAssertEqual(color, .red)
     }
+    
+    func testRazeColorsAreEqual() {
+        let color = RazeCore.colorFromHexString ("006736")
+        XCTAssertEqual(color, RazeCore.razeColor)
+    }
 }
